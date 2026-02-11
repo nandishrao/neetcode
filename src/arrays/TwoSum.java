82852b03ab0e93f6abaca35fe1978d26d9dd2bc0
Nandish Rao <nandishhraoo@gmail.com>
2026-02-11 22:39:58 +0530

    05: Two Sum problem in Java using Map and HashMap

---
+++ src/arrays/TwoSum.java
@@ -0,0 +1,21 @@
+package arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                System.out.println("Answer: " + map.get(complement) + " " + i);
+                return;
+            }
+            map.put(nums[i], i);
+        }
+    }
+}
