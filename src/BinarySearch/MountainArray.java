8638098d4514366967f188977991ab6b4a7852c2
Nandish Rao <nandishhraoo@gmail.com>
2026-03-13 21:12:10 +0530

    finding the peak of an array where the 1st part of the is sorted in asc and 2nd part is desc using Binary Search.

---
+++ src/BinarySearch/MountainArray.java
@@ -0,0 +1,32 @@
+package BinarySearch;
+
+public class MountainArray {
+    public static void main(String[] args) {
+    int[] arr={0,10,5,2};
+    int ans = FindingPeak(arr);
+    System.out.print(ans);
+    }
+    static int FindingPeak(int [] arr){
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start <  end){
+            int mid = start + (end - start)/2;
+            if(arr[mid] > arr[mid+1]){
+                //you're in the decreasing part of the array
+                //this might be the answer, but we must look in the left.
+                //this is why end is not mid-1
+                end = mid;
+            }else{
+                //you're at the ascending part of the array
+                start = mid +1 ; //because we know that mid+1 element > mid-element
+
+            }
+        }
+        //in the end start == end and pointing to the answer(largest answer) cause of the two conditions above
+        //start and end are always trying to find the largest element in the array, hence when they point the same one element
+        //that's the largest element
+        return start;
+    }
+
+}
